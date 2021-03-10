428d8e61c11e9b17bf5d81782f28f777c0687606
tanay-pingalkar <tpingalkar@gmail.com>
2021-03-10 19:27:42 +0530

    made nav bar responsive

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -2,14 +2,17 @@ import "../styles/nav.scss";
 import Bell from "../svg/bell";
 import Home from "../svg/home";
 import Search from "../svg/search";
+import logo from "../media/Wallpaper-Malavida-Green-Top-Image.jpg";
 const Nav = () => {
   return (
     <div className="nav">
+      <img alt="profile" src={logo} className="profile"></img>
       <div className="toggler">
         <Search></Search>
         <Home></Home>
         <Bell></Bell>
       </div>
+      <div className="add">+</div>
     </div>
   );
 };
