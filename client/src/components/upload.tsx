e203fcb81c862555f2d129b1acaad4ed0d4983ec
tanay-pingalkar <tpingalkar@gmail.com>
2021-03-13 16:34:28 +0530

    some changes

--- client/src/components/upload.tsx
+++ client/src/components/upload.tsx
@@ -4,21 +4,32 @@ import { client } from "../graphql/client";
 import { ADD_FRAME } from "../graphql/mutations/addFrame";
 import UploadSvg from "../svg/upload";
 import "../styles/upload.scss";
+import ReactLoading from "react-loading";
 
 const Upload = () => {
   const [title, setTitle] = useState("");
   const [description, setdescription] = useState("");
   const [img, setImg] = useState<any>();
+  const [base64, setBase64] = useState<string | ArrayBuffer | null>("");
   const userInfo = useSelector((states: any) => states.userInfo);
-  console.log(userInfo);
+  const [isLoading, setLoading] = useState(false);
+
   const handleSubmit = async () => {
-    const res = await client.request(ADD_FRAME, {
-      id: parseInt(userInfo.id),
-      title: title,
-      description: description,
-      file: img,
-    });
-    console.log(res);
+    setLoading(true);
+    try {
+      const res = await client.request(ADD_FRAME, {
+        id: parseInt(userInfo.id),
+        title: title,
+        description: description,
+        file: img,
+      });
+      alert(res.addFrame.msg);
+    } catch (err) {
+      setLoading(false);
+      console.log(err);
+    }
+
+    setLoading(false);
   };
   console.log(img);
   return (
@@ -30,12 +41,21 @@ const Upload = () => {
           placeholder="title.."
           className="title"
         ></input>
-        <span>
+        <span style={{ backgroundImage: `url(${base64})` }}>
           <UploadSvg></UploadSvg>
           <input
             type="file"
             className="image-input"
-            onChange={(e) => setImg(e.target!.files![0])}
+            onChange={(e) => {
+              const file = e.target!.files![0];
+              setImg(file);
+              var reader = new FileReader();
+              reader.onloadend = function () {
+                setBase64(reader.result);
+              };
+
+              reader.readAsDataURL(file);
+            }}
           ></input>
         </span>
         <textarea
@@ -43,7 +63,19 @@ const Upload = () => {
           className="description"
           onChange={(e) => setdescription(e.target.value)}
         ></textarea>
-        <button onClick={handleSubmit}>Upload</button>
+        <button onClick={handleSubmit}>
+          {isLoading ? (
+            <ReactLoading
+              type={"bars"}
+              color={"black"}
+              height={"30px"}
+              width={"40px"}
+              className="loader"
+            />
+          ) : (
+            <>Upload</>
+          )}
+        </button>
       </div>
     </div>
   );
